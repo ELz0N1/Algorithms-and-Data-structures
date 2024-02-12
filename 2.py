b06dc80cdc67e2b2d96c9bd78261e2ff64b480ae
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-02-12 21:30:15 +0700

    Add files via upload
    Karatsuba

---
+++ 2.py
@@ -0,0 +1,17 @@
+def karatsuba(p: int, q: int):
+    if len(str(p)) == 1 or len(str(q)) == 1:
+        return p * q
+    else:
+        max_len = max(len(str(p)), len(str(q)))
+        m2 = max_len // 2
+
+        left_p = p // (10 ** m2)
+        right_p = p % (10 ** m2)
+        left_q = q // (10 ** m2)
+        right_q = q % (10 ** m2)
+
+        left_pq = karatsuba(left_p, left_q)
+        right_pq = karatsuba(right_p, right_q)
+        lprq_and_rplq = karatsuba((left_p + right_p), (left_q + right_q)) - left_pq - right_pq
+
+        return (left_pq * (10 ** (2 * m2))) + ((lprq_and_rplq) * (10 ** m2)) + right_pq
