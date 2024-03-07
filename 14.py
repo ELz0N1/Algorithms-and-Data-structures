3158ac023f50665fb0429898e1b8d28a6e8a3769
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-03-07 22:57:34 +0700

    Add files via upload

---
+++ 14.py
@@ -0,0 +1,34 @@
+from random import randint
+
+
+def kthQuickSort(array, low, high, K):
+    if len(array) == 1: return array[low]
+
+    i, j = low, high - 1
+    pivot = array[randint(low, high - 1)]
+
+    while True:
+        while i <= j and array[i] < pivot:
+            i += 1
+        while i <= j and array[j] > pivot:
+            j -= 1
+        if i >= j: break
+
+        array[i], array[j] = array[j], array[i]
+        i += 1
+        j -= 1
+
+    if (array[j] == pivot and j == K) or (array[i] == pivot and i == K):
+        return pivot
+
+    if i > K:
+        return kthQuickSort(array, low, i, K)
+    else:
+        return kthQuickSort(array, j + 1, high, K)
+
+
+if __name__ == '__main__':
+    nums = [randint(1, 100) for i in range(randint(1, 50))]
+    result = kthQuickSort(nums, 0, len(nums), len(nums) // 2)
+    print(f"{result} is median for an array:")
+    print(sorted(nums))
