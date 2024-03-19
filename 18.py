251a2672c71cbe164d600a99eda69e3d6ed43544
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-03-19 13:21:47 +0700

    Update 18.py

--- 18.py
+++ 18.py
@@ -3,28 +3,30 @@ def is_operand(char):
 
 
 def precedence(char):
-    if char == '|':
+    if char == "||":
+        return 10
+    elif char == '&&':
+        return 9
+    elif char == '|':
+        return 8
+    elif char == '^':
         return 7
     elif char == '&':
         return 6
-    # elif char == '<<' or char == '>>':
-    #     return 5
-    elif (char == '<' or char == '<=') or (char == '>' or char == '>='):
+    elif char == "==" or char == "!=":
         return 5
-    elif char == '==' or char == '!=':
-        return 4
-    elif char == '^':
+    elif char == '+' or char == '-':
         return 3
     elif char == '/' or char == '*' or char == '%':
         return 2
-    elif char == '+' or char == '-':
+    elif char == '!' or char == '~':
         return 1
     else:
         return -1
 
 
 def associativity(char):
-    if char == '^':
+    if char == '!' or char == '~' or char == '&':
         return 'R'
     return 'L'
 
@@ -33,20 +35,26 @@ def infix_to_postfix(expression):
     stack = []
     result = []
 
-    for element in expression:
+    expression.reverse()
+    while expression:
+        element = expression.pop()
         if is_operand(element):
             result.append(element)
+
         elif element == '(':
             stack.append(element)
+
         elif element == ')':
             while stack and stack[-1] != '(':
                 result.append(stack.pop())
-            stack.pop()  # pop '('
+            stack.pop()
+
         else:
             while stack and (precedence(element) < precedence(stack[-1]) or
                              (precedence(element) == precedence(stack[-1]) and associativity(element) == 'L')):
                 result.append(stack.pop())
             stack.append(element)
+
 
     while stack:
         result.append(stack.pop())
@@ -55,8 +63,13 @@ def infix_to_postfix(expression):
 
 
 if __name__ == "__main__":
-    exp = "1-5^3*2"
+    string = "( 1 - 5 ) ^ 3 * 2"
+    exp = string.split()
     print(infix_to_postfix(exp))
 
-    exp = "(1-5)&&(3*2)"
+    string = "9 + 3 * ( ( 2 % 5 ) - 1 ) ^ ( 6 + 7 * 4 ) - 8"
+    exp = string.split()
+    print(exp)
     print(infix_to_postfix(exp))
+
+ 
