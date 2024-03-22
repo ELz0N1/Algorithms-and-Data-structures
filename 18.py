d70b038f2d2e154a852b3800b3962a85f5772750
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-03-22 23:52:04 +0700

    Update 18.py

--- 18.py
+++ 18.py
@@ -2,39 +2,45 @@ def is_operand(char):
     return ('a' <= char <= 'z') or ('A' <= char <= 'Z') or ('0' <= char <= '9')
 
 
-def precedence(char):
-    if char == "||":
-        return 10
-    elif char == '&&':
-        return 9
-    elif char == '|':
-        return 8
-    elif char == '^':
-        return 7
-    elif char == '&':
-        return 6
-    elif char == "==" or char == "!=":
-        return 5
-    elif char == '+' or char == '-':
-        return 3
-    elif char == '/' or char == '*' or char == '%':
-        return 2
-    elif char == '!' or char == '~':
-        return 1
-    else:
-        return -1
+def get_precedence(operation):
+    precedence = {
+        '(': 0,
+        ')': 0,
+        '+': 1,
+        '-': 1,
+        '*': 2,
+        '/': 2,
+        '%': 2,
+        '&': 3,
+        '|': 3,
+        '^': 3,
+        '<<': 4,
+        '>>': 4,
+        '<': 5,
+        '<=': 5,
+        '>': 5,
+        '>=': 5,
+        '==': 6,
+        '!=': 6,
+        '&&': 7,
+        '||': 8,
+        '!': 9,
+        '~': 9
+    }
+    return precedence[operation]
 
 
-def associativity(char):
-    if char == '!' or char == '~' or char == '&':
+def associativity(operation):
+    if operation == '!' or operation == '~' or operation == '&':
         return 'R'
     return 'L'
 
 
-def infix_to_postfix(expression):
+def infix_to_postfix(string):
     stack = []
     result = []
 
+    expression = string.split()
     expression.reverse()
     while expression:
         element = expression.pop()
@@ -50,11 +56,10 @@ def infix_to_postfix(expression):
             stack.pop()
 
         else:
-            while stack and (precedence(element) < precedence(stack[-1]) or
-                             (precedence(element) == precedence(stack[-1]) and associativity(element) == 'L')):
+            while stack and (get_precedence(element) <= get_precedence(stack[-1]) or
+                             (get_precedence(element) == get_precedence(stack[-1]) and associativity(element) == 'L')):
                 result.append(stack.pop())
             stack.append(element)
-
 
     while stack:
         result.append(stack.pop())
@@ -63,13 +68,12 @@ def infix_to_postfix(expression):
 
 
 if __name__ == "__main__":
-    string = "( 1 - 5 ) ^ 3 * 2"
-    exp = string.split()
-    print(infix_to_postfix(exp))
+    assert infix_to_postfix("( 1 - 5 ) ^ 3 * 2") == "15-3^2*"
+    assert infix_to_postfix("2 * 1 / 1 * 2 + 1") == "21*1/2*1+"
+    assert infix_to_postfix("4 * ( 1 + 9 )") == "419+*"
+    assert infix_to_postfix("6 * ( 0 + 7 ) / 2") == "607+*2/"
+    assert infix_to_postfix("2 * ( ( 9 + 6 ) / 3 )") == "296+3/*"
+    assert infix_to_postfix("c | ( b & a )") == "cba&|"
+    assert infix_to_postfix("! x | ( y & z )") == "x!yz&|"
 
-    string = "9 + 3 * ( ( 2 % 5 ) - 1 ) ^ ( 6 + 7 * 4 ) - 8"
-    exp = string.split()
-    print(exp)
-    print(infix_to_postfix(exp))
-
- 
+    print("All tests passed")
