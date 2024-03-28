1730fbd6c8303585c243148318819d9f1c7d8bd1
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-03-28 09:49:03 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -11,9 +11,10 @@ def divide(number: int, divider: int):
         int_digit = 0
         current = int(digit) + remainer * 10
 
-        while current >= divider:
-            current -= divider
-            int_digit += 1
+        for i in range(9, 0, -1):
+            if current >= i * divider:
+                current = current - (i * divider)
+                int_digit += i
 
         integer = integer * 10 + int_digit
         remainer = current
@@ -28,4 +29,3 @@ if __name__ == "__main__":
         assert (A // B, A % B) == divide(A, B)
 
     print("All tests passed")
-
