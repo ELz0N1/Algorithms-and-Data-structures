43698836f9d70372d4a40d99b947ea7e2069e283
Danil Zhukov <91378756+ELz0N1@users.noreply.github.com>
2024-04-29 11:37:40 +0700

    added tests

--- 14.py
+++ 14.py
@@ -1,11 +1,6 @@
 from random import randint
 
 
-def naiveImp(array, K):
-    array.sort()
-    return array[K]
-
-
 def kthQuickSort(array, low, high, K):
     if len(array) == 1: return array[low]
 
@@ -29,16 +24,42 @@ def kthQuickSort(array, low, high, K):
     if i > K:
         return kthQuickSort(array, low, i, K)
     else:
-        return kthQuickSort(array, j + 1, high, K)
+        return kthQuickSort(array, i, high, K)
+
+
+def tests():
+    array = [41, 6, 22, 46, 53, 26, 76, 92, 37, 82, 79, 1, 60, 84, 91, 53, 7, 84]
+    assert kthQuickSort(array, 0, len(array), len(array) // 2) == 53
+
+    array = [85, 62, 64, 16, 19, 19, 22, 85, 55]
+    assert kthQuickSort(array, 0, len(array), len(array) // 2) == 55
+
+    array = [94, 15, 32]
+    assert kthQuickSort(array, 0, len(array), len(array) // 2) == 32
+
+    array = [81, 59, 43, 87, 14, 7, 97, 18, 1, 66, 18, 83, 24]
+    assert kthQuickSort(array, 0, len(array), len(array) // 2) == 43
+
+    array = [2, 35, 29, 39, 64, 59, 3, 17]
+    assert kthQuickSort(array, 0, len(array), len(array) // 2) == 35
+
+    array = [4, 20, 50, 46, 82, 90, 1, 85, 48, 94]
+    assert kthQuickSort(array, 0, len(array), len(array) // 2) == 50
+
+    array = [27, 76, 99, 32, 70, 59, 64, 60, 82, 14, 56]
+    assert kthQuickSort(array, 0, len(array), len(array) // 2) == 60
+
+    array = [28, 14, 52, 53, 28, 97, 36, 2, 69, 84, 15, 65, 37, 60, 94, 24, 79]
+    assert kthQuickSort(array, 0, len(array), len(array) // 2) == 52
+
+    array = [46, 1, 48, 21, 48, 51, 39, 85, 9, 42, 97, 1, 3, 43, 93, 19]
+    assert kthQuickSort(array, 0, len(array), len(array) // 2) == 43
+
+    array = [45, 65, 87, 3, 26, 95, 65, 58]
+    assert kthQuickSort(array, 0, len(array), len(array) // 2) == 65
+
+    print("All tests passed!")
 
 
 if __name__ == '__main__':
-    for j in range(101):
-        nums = [randint(1, 100) for i in range(randint(1, 50))]
-
-        result = kthQuickSort(nums, 0, len(nums), len(nums) // 2)
-        naive_result = naiveImp(nums, len(nums) // 2)
-
-        assert result == naive_result
-
-    print("All tests passed")
+    tests()
